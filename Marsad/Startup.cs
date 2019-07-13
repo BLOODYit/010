64807a0f7e846f9cd4a2eff3d682b597401b56b4
ramymibrahim <ramymibrahim@yahoo.com>
2019-07-13 21:41:31 +0200

    First Commit

---
+++ Marsad/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Marsad.Startup))]
+namespace Marsad
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
